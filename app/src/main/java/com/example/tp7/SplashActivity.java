0109639ed92b52946898520d8b4739613e7b28a3
Hassan HKIN <hakat2003@yahoo.fr>
2020-06-01 10:53:12 +0200

    add splash screen

---
+++ app/src/main/java/com/example/tp7/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.example.tp7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+
+            }
+        }, 2000);
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
